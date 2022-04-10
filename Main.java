f5f95a3263bd95050ba2e1dbc3d4477a3f0c2d34
Teoman Akgün <teomanakgn84@gmail.com>
2022-04-10 12:36:11 +0300

    versiyon2

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
         return x+ y;
     }
 
+    public static void bolme(int x,int y) {
+            System.out.println( x/ y);  
+    }
 
     public static void main(String[] args) {
     }
-}   
+}    
